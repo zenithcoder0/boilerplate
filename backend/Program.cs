e0ac9a734fd63465ae8580fe110ec312b76ca1cc
zenithcoder0 <zenithcoder0@gmail.com>
2025-06-19 14:57:53 -0500

    Boilerplate finish

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,8 +3,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("AllowFrontend", policy => 
+    {
+        policy.WithOrigins(
+            "http://localhost:5173"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
+app.UseCors("AllowFrontend");
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
